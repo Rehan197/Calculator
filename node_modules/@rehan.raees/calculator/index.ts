e24b1d9c84232736f2990480ee319221322b993c
rehan.raees <rehan.raees1@gmail.com>
2023-10-13 16:13:21 +0500

    Calculator Complete

---
+++ node_modules/@rehan.raees/calculator/index.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answers : {
+    NumberOne:number
+    NumberTwo:number
+    Operator:string
+} = await inquirer.prompt([
+    {
+        type:"number",
+        name:"NumberOne",
+        message:"Kindly Enter Your First No"
+    },
+    {
+        type:"number",
+        name:"NumberTwo",
+        message:"Kindly Enter Your Second No"
+    },
+    {
+        type:"list",
+        name:"Operator",
+        choices :["+","-","*","/"],
+        message:"Select Operator:"
+    },
+
+])
+
+const {NumberOne, NumberTwo, Operator} = answers;
+
+if(NumberOne && NumberTwo && Operator) {
+    let result:number=0;
+    if (Operator==="+"){
+        result=NumberOne+NumberTwo
+    } else if (Operator==="-"){
+        result=NumberOne-NumberTwo
+    } if (Operator==="/"){
+        result=NumberOne/NumberTwo
+    } if (Operator==="*"){
+        result=NumberOne*NumberTwo
+    }
+
+    console.log("your result is:", result)
+
+} else{
+
+    console.log("Kindly Enter Valid input")
+}
+
